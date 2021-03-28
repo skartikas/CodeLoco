301648c10f0221de7bb847d15d097270ce3f2e4b
Kartik <skartikecs@gmail.com>
2021-03-28 13:02:19 -0500

    initialize empty project

---
+++ CodeLoco/CodeLocoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeLocoApp.swift
+//  CodeLoco
+//
+//  Created by Kartikey Singh on 3/28/21.
+//
+
+import SwiftUI
+
+@main
+struct CodeLocoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
