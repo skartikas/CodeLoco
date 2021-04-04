d6b4997dafa99182d11957396b5bcc8b6d436dab
Nemir Gandhi <nemir.gandhi@gmail.com>
2021-04-03 20:15:20 -0400

    Login Functionality

--- CodeLoco/AppDelegate.swift
+++ CodeLoco/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "lZCjnuKRPpLIuM1EVUNF9sjDf9YBFlobjdFfpPXW" // <- UPDATE
+                    $0.clientKey = "A0lMhSRRkaoWmm1CgpmSV2AoSikgIJpQI0CPLfCZ" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+        
         // Override point for customization after application launch.
         return true
     }
