61af0b6b4c79a2c72c89edb994d780ae05046467
dikshitakejriwal <dikshita.kejriwal@gmail.com>
2021-04-04 12:40:23 -0400

    Completed Module Home page

--- CodeLoco/HomeViewController.swift
+++ CodeLoco/HomeViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var module1View: UIImageView!
+    @IBOutlet weak var module2View: UIImageView!
+    @IBOutlet weak var module3View: UIImageView!
+    @IBOutlet weak var module4View: UIImageView!
+    
+    @IBOutlet weak var module1Label: UILabel!
+    @IBOutlet weak var module2Label: UILabel!
+    @IBOutlet weak var module3Label: UILabel!
+    @IBOutlet weak var module4Label: UILabel!
+    @IBOutlet weak var comingSoonLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //round the radius
+        module1View.layer.cornerRadius = 20
+        module2View.layer.cornerRadius = 20
+        module3View.layer.cornerRadius = 20
+        module4View.layer.cornerRadius = 20
         // Do any additional setup after loading the view.
     }
     
