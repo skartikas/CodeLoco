aad58353a54a1289fd36acc0239a6541f2e4f561
Kartik <skartikecs@gmail.com>
2021-04-10 13:00:15 -0500

    add profile settings

--- CodeLoco/ProfileSettingsViewController.swift
+++ CodeLoco/ProfileSettingsViewController.swift
@@ -10,9 +10,8 @@ import AlamofireImage
 import Parse
 
 class ProfileSettingsViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
-    @IBOutlet weak var imageView: UIImageView!
  
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,41 +19,42 @@ class ProfileSettingsViewController: UIViewController, UIImagePickerControllerDe
         // Do any additional setup after loading the view.
     }
     
-
     @IBAction func onSubmit(_ sender: Any) {
         let currentUser = PFUser.current()
-        if currentUser != nil {
-            let imageData = imageView.image!.pngData()
-            let file = PFFileObject(data: imageData!)
-            
-            currentUser!["profile_image"] = file
-            
-            currentUser?.saveInBackground{(success, error) in
-                if success{
-                    self.dismiss(animated: true, completion: nil)
+                if currentUser != nil {
+                    let imageData = imageView.image!.pngData()
+                    let file = PFFileObject(data: imageData!)
+        
+                    currentUser!["profile_image"] = file
+        
+                    currentUser?.saveInBackground{(success, error) in
+                        if success{
+                            self.dismiss(animated: true, completion: nil)
+                        }
+                        else{
+                            print("Error")
+                        }
+                    }
                 }
-                else{
-                    print("Error")
-                }
-            }
-        }
     }
+    
     
     @IBAction func onCameraButton(_ sender: Any) {
         let picker = UIImagePickerController()
-        picker.delegate = self
-        picker.allowsEditing = true
+                picker.delegate = self
+                picker.allowsEditing = true
         
-        if UIImagePickerController.isSourceTypeAvailable(.camera){
-            picker.sourceType = .camera
-        }else{
-            picker.sourceType = .photoLibrary
-        }
+                if UIImagePickerController.isSourceTypeAvailable(.camera){
+                    picker.sourceType = .camera
+                }else{
+                    picker.sourceType = .photoLibrary
+                }
         
         
-        present(picker, animated: true, completion: nil)
+                present(picker, animated: true, completion: nil)
+       
     }
-    
+
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
